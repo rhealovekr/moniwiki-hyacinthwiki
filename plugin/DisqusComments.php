cd4b6b64a2c71cb081a8c2a8226049df15664deb
rhealovekr <rhealove01@gmail.com>
2014-09-12 17:11:26 +0900

    Added [[DisqusComments]] Macro

--- plugin/DisqusComments.php
+++ plugin/DisqusComments.php
@@ -1,8 +1,9 @@
 <?php
 // from http://www.sitepoint.com/examples/phpxml/sitepointcover-oo.php.txt
 // Public Domain ?
-// $Id: RecentComments.php,v 1.0 2014/06/02 14:32:44 rhealove Exp $
-class DisqusRecentComments {
+// $Id: DisqusComments.php,v 1.0 2014/06/02 14:32:44 hyacinth Exp $
+
+class DisqusComments {
 
    var $insideitem = false;
    var $tag = "";
@@ -12,11 +13,10 @@ class DisqusRecentComments {
    var $link = "";
    var $date = "";
    var $date_format = "F jS, Y";
-   #var $date_format = "m-d [h:i a] Y";
    var $cnt = 5;
    var $show_opt = "titleonly";
 
-   function DisqusRecentComments() {
+   function DisqusComments() {
    }
 
    function startElement($parser, $tagName, $attrs) {
@@ -35,16 +35,13 @@ class DisqusRecentComments {
            $this->end_count++;
            if ($this->status) print "[$this->status] ";
            if ($this->show_opt == "all") {
-             printf("<div class=\"disqus-recent-comments\">%s -- %s ", $this->description, $this->creator);
+             printf("<div class=\"disqus-comments\">%s -- %s ", $this->description, $this->creator);
            }
-           printf("<a href='%s' target='_content'>%s</a>",
+           printf("<a href='%s'>%s</a>",
              trim($this->link),
              htmlspecialchars(trim($this->title)));
            
            if ($this->date) {
-             # <pubDate>Fri, 14 Dec 2012 01:30:17 -0000</pubDate>
-             #$zone=str_replace(":","",substr($date,19));
-             #$time=strtotime(substr($date,0,19).$zone);
              $time=str_replace("-0000","",$this->date);
              $date=date($this->date_format, strtotime($time)+50400);
              printf(" <span class=\"blog-user\">%s %s </span><br/>\n", htmlspecialchars(trim($date)), $this->tz_off);
@@ -87,19 +84,19 @@ class DisqusRecentComments {
                $this->status .= $data;
                break;
            }
-# TITLE/LINK/DESCRIPTION/DC:CREATOR/PUBDATE
-           #print $this->tag."/";
+           //TITLE/LINK/DESCRIPTION/DC:CREATOR/PUBDATE
+           //print $this->tag."/";
        }
    }
 }
 
-function macro_RecentComments($formatter,$value) {
+function macro_DisqusComments($formatter,$value) {
   global $DBInfo;
   $this->end_count = 0;
 
   $xml_parser = xml_parser_create();
 
-  $rss_parser = new DisqusRecentComments();
+  $rss_parser = new DisqusComments();
   xml_set_object($xml_parser,$rss_parser);
   xml_set_element_handler($xml_parser, "startElement", "endElement");
   xml_set_character_data_handler($xml_parser, "characterData");
@@ -114,32 +111,32 @@ function macro_RecentComments($formatter,$value) {
   $key=_rawurlencode($username);
 
   $cache= new Cache_text("rss");
-  # refresh rss each 5 minutes (60*5)
+  // refresh rss cache every 5 minutes (60*5)
   if (!$cache->exists($key) or (time() > $cache->mtime($key) + 300 )) {
     $URL_parsed = parse_url("http://".$username.".disqus.com/latest.rss");
 
     $host = $URL_parsed["host"];
     $port = $URL_parsed["port"];
     if ($port == 0)
-        $port = 80;
+      $port = 80;
 
     $path = $URL_parsed["path"];
     if ($URL_parsed["query"] != "")
-        $path .= "?".$URL_parsed["query"];
+      $path .= "?".$URL_parsed["query"];
 
     $out = "GET $path HTTP/1.0\r\nHost: $host\r\n\r\n";
     $fp = fsockopen($host, $port, $errno, $errstr, 30);
 
     if (!$fp)
-      return ("[[RSS(ERR: not a valid URL! $value)]]");
+      return ("[[DisqusComments(ERR: not a valid URL! $value)]]");
 
-    fputs ($fp, $out);
+    fputs($fp, $out);
     $body = false;
-    while (!feof($fp)) {
+    while(!feof($fp)) {
       $data = fgets($fp, 4096);
-      if ( $body )
+      if ($body)
         $xml_data .= $data;
-      if ( $data == "\r\n" )
+      if ($data == "\r\n")
         $body = true;
     }
 
@@ -152,21 +149,20 @@ function macro_RecentComments($formatter,$value) {
   preg_match("/\sencoding=?(\"|')([^'\"]+)/",$line,$match);
   if ($match) $charset=strtoupper($match[2]);
   else $charset='UTF-8';
-  # override $charset for php5
+  // override $charset for php5
   if ((int)phpversion() >= 5) $charset='UTF-8';
 
   ob_start();
   $ret= xml_parse($xml_parser, $xml_data);
 
   if (!$ret)
-    return (sprintf("[[RSS(XML error: %s at line %d)]]",  
+    return (sprintf("[[DisqusComments(XML error: %s at line %d)]]",  
       xml_error_string(xml_get_error_code($xml_parser)),  
       xml_get_current_line_number($xml_parser)));
   $out=ob_get_contents();
   ob_end_clean();
   xml_parser_free($xml_parser);
 
-  #  if (strtolower(str_replace("-","",$options['oe'])) == 'euckr')
   if (function_exists('iconv') and strtoupper($DBInfo->charset) != $charset)
     $new=iconv($charset,$DBInfo->charset,$out);
   if ($new) return $new;
